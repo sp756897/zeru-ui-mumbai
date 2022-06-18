0814614a5eb9ee934c948d66e1635fe333072aea
Aviroxi <gurugkgurukiran@gmail.com>
2022-06-18 15:47:13 +0530

    update in staking tab

--- frontend/src/components/Staking.js
+++ frontend/src/components/Staking.js
@@ -1,7 +1,45 @@
+import { Card, Col, Row } from 'antd'
+import ButtonGroup from 'antd/lib/button/button-group'
 import React from 'react'
 
 export default function Staking() {
   return (
-    <div>Staking</div>
+    <div>
+        <Row>
+            <Col span={12}>
+                Funds in safety mode<br/>2,345,478.00
+            </Col>
+            <Col span={12}>
+                Total emmission per day<br/>453.67
+            </Col>
+        </Row>
+        <Card>
+            Stake ZERU
+            <Row>
+                <Col span={6}>
+                    ZERU
+                </Col>
+                <Col span={6}>
+                    Staking APY<br/>8.20%
+                </Col>
+                <Col span={6}>
+                    Max Slashing<br/>30.00%
+                </Col>
+                <Col span={6}>
+                    <Button>
+                        Stake
+                    </Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={12}>
+                    Staked ZERU<br/>10
+                </Col>
+                <Col span={12}>
+                    Claimable ZERU<br/>2
+                </Col>
+            </Row>
+        </Card>
+    </div>
   )
 }
