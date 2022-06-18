c1e1d59096e19fba8b669a6c5cff1a712b1d5f2e
Aviroxi <gurugkgurukiran@gmail.com>
2022-06-18 16:22:28 +0530

    added the staking page

--- frontend/src/components/Staking.js
+++ frontend/src/components/Staking.js
@@ -1,45 +1,69 @@
 import { Card, Col, Row } from 'antd'
-import ButtonGroup from 'antd/lib/button/button-group'
-import React from 'react'
+import { Button } from 'antd';
+import { Typography } from 'antd';
 
+import React from 'react'
+import "../styles/styles.css"
+
+const flexcss = { display: 'flex', alignItems: 'center', justifyContent: 'space-around', textAlign: 'center' }
+
+const { Title } = Typography;
 export default function Staking() {
-  return (
-    <div>
-        <Row>
-            <Col span={12}>
-                Funds in safety mode<br/>2,345,478.00
-            </Col>
-            <Col span={12}>
-                Total emmission per day<br/>453.67
-            </Col>
-        </Row>
-        <Card>
-            Stake ZERU
-            <Row>
-                <Col span={6}>
-                    ZERU
+    return (
+        <div>
+            <Row className='textaligncenter'>
+                <Col span={12} className='text'>
+                    Funds in safety mode
+                    <p className='value'>2,345,478.00</p>
                 </Col>
-                <Col span={6}>
-                    Staking APY<br/>8.20%
-                </Col>
-                <Col span={6}>
-                    Max Slashing<br/>30.00%
-                </Col>
-                <Col span={6}>
-                    <Button>
-                        Stake
-                    </Button>
+                <Col span={12} className='text'>
+                    Total emmission per day
+                    <p className='value'>453.67</p>
                 </Col>
             </Row>
-            <Row>
-                <Col span={12}>
-                    Staked ZERU<br/>10
-                </Col>
-                <Col span={12}>
-                    Claimable ZERU<br/>2
-                </Col>
-            </Row>
-        </Card>
-    </div>
-  )
+            <Card style={{ maxWidth: '1000px', marginLeft: 'auto', marginRight: 'auto' }}>
+                <Title level={2}>Stake ZERU</Title>
+                <br />
+                <Row style={flexcss}>
+                    <Col span={6}>
+                        <Title level={5}>ZERU</Title>
+                    </Col>
+                    <Col span={6}>
+                        Staking APY
+                        <Title level={5}>8.20%</Title>
+
+                    </Col>
+                    <Col span={6}>
+                        Max Slashing
+                        <Title level={5}>30.00%</Title>
+
+                    </Col>
+                    <Col span={6}>
+                        <Button type="primary">
+                            Stake
+                        </Button>
+                    </Col>
+                </Row>
+                <Row style={flexcss}>
+                    <Col span={12}>
+                        <Card style={{ height:'200px' }}>
+                            <h2 level={3}>Staked ZERU</h2>
+                            <Title level={2}>10</Title>
+                            
+                        </Card>
+
+                    </Col>
+                    <Col span={12} >
+                    <Card style={{ height:'200px' }}>
+                            <h2>Claimable ZERU</h2>
+                            <Title level={2}>2</Title>
+                            <Button type='primary'>
+                                Claim ZERU
+                            </Button>
+                        </Card>
+                    </Col>
+                </Row>
+            </Card>
+        </div>
+    )
 }
